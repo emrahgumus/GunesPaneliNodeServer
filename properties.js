35641fc98f1aed54be97fab3cdae8d2077540ca5
Emrah Gümüş <emrahgumus34@gmail.com>
2016-12-16 19:31:42 +0300

    gunes paneli server dosyalar

---
+++ properties.js
@@ -0,0 +1,18 @@
+/**
+ * Created by cebbus on 15.03.2016.
+ */
+var properties = {};
+
+properties.http = {
+    port: '8080'
+};
+
+properties.db = {
+    server: 'localhost',
+    database: 'GunesPaneli',
+    username: null,
+    password: null,
+    port: '27017'
+};
+
+module.exports = properties;
